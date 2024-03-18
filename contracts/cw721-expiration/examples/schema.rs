51e65ca94beef9c386d77acde620aedb6f162c12
mr-t <mr-t@arkprotocol.io>
2024-03-18 21:06:51 +0100

    cleanup

--- contracts/cw721-expiration/examples/schema.rs
+++ contracts/cw721-expiration/examples/schema.rs
@@ -1,5 +1,4 @@
 use cosmwasm_schema::write_api;
-use cosmwasm_std::Empty;
 use cw721_base::{DefaultOptionCollectionMetadataExtension, DefaultOptionNftMetadataExtension};
 use cw721_expiration::msg::{InstantiateMsg, QueryMsg};
 
